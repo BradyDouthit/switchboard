aa5a05e45bd27701ba0aa072140ca59d4abee01a
Brady Douthit <brady.douthit99@gmail.com>
2024-11-17 21:18:13 -0500

    add Run() func at end of command body

--- examples/main.go
+++ examples/main.go
@@ -10,20 +10,36 @@ func main() {
 	app := switchboard.New()
 
 	app.Command("greet", func(c *switchboard.Command) {
-		c.Flag("name", "First name", true,
+		var greeting string
+		var fullName string
+
+		c.Flag("g", "greeting", "Greeting to use", true,
 			func(value string, _ interface{}) switchboard.FlagResult {
-				return switchboard.FlagResult{
-					Value: value,
+				greeting = value
+				if greeting == "" {
+					greeting = "Hello"
 				}
+				return switchboard.FlagResult{Value: value}
 			})
 
-		c.Flag("L", "Last name", false,
-			func(value string, prevResult interface{}) switchboard.FlagResult {
-				firstName := prevResult.(string)
-				fullName := fmt.Sprintf("%s %s", firstName, value)
-				fmt.Printf("Hello %s\n", fullName)
+		c.Flag("n", "name", "First name", true,
+			func(value string, _ interface{}) switchboard.FlagResult {
+				fullName = value
+				return switchboard.FlagResult{Value: value}
+			})
+
+		c.Flag("l", "lastname", "Last name", false,
+			func(value string, _ interface{}) switchboard.FlagResult {
+				fmt.Println("Last Name!", value)
+				if value != "" {
+					fullName += " " + value
+				}
 				return switchboard.FlagResult{Value: fullName}
 			})
+
+		c.Run(func() {
+			fmt.Printf("%s %s\n", greeting, fullName)
+		})
 	})
 
 	app.Run()
--- switchboard.go
+++ switchboard.go
@@ -12,7 +12,8 @@ type FlagResult struct {
 }
 
 type Flag struct {
-	Name        string
+	Short       string
+	Long        string
 	Description string
 	Required    bool
 	processor   func(string, interface{}) FlagResult
@@ -20,8 +21,9 @@ type Flag struct {
 
 type Command struct {
 	Name  string
-	Flags map[string]*Flag
-	order []string // Maintain flag processing order
+	Flags map[string]*Flag // Key is the long name
+	order []string
+	runFn func()
 }
 
 type CLI struct {
@@ -44,15 +46,20 @@ func (c *CLI) Command(name string, fn func(*Command)) {
 	c.commands[name] = cmd
 }
 
-func (c *Command) Flag(name string, description string, required bool, processor func(string, interface{}) FlagResult) {
+func (c *Command) Flag(short, long, description string, required bool, processor func(string, interface{}) FlagResult) {
 	flag := &Flag{
-		Name:        name,
+		Short:       short,
+		Long:        long,
 		Description: description,
 		Required:    required,
 		processor:   processor,
 	}
-	c.Flags[name] = flag
-	c.order = append(c.order, name)
+	c.Flags[long] = flag
+	c.order = append(c.order, long)
+}
+
+func (c *Command) Run(fn func()) {
+	c.runFn = fn
 }
 
 func (c *CLI) Run() {
@@ -62,13 +69,17 @@ func (c *CLI) Run() {
 	}
 
 	if cmd, ok := c.commands[args[0]]; ok {
-
 		flagValues := make(map[string]string)
 		requiredFlags := make(map[string]bool)
 
-		for flagName, flag := range cmd.Flags {
+		// Create short name lookup
+		shortToLong := make(map[string]string)
+		for longName, flag := range cmd.Flags {
+			if flag.Short != "" {
+				shortToLong[flag.Short] = longName
+			}
 			if flag.Required {
-				requiredFlags[flagName] = false
+				requiredFlags[longName] = false
 			}
 		}
 
@@ -76,7 +87,16 @@ func (c *CLI) Run() {
 		for i := 1; i < len(args); i++ {
 			arg := args[i]
 			if strings.HasPrefix(arg, "-") {
-				flagName := strings.TrimPrefix(arg, "-")
+				var flagName string
+				if strings.HasPrefix(arg, "--") {
+					flagName = strings.TrimPrefix(arg, "--")
+				} else {
+					shortName := strings.TrimPrefix(arg, "-")
+					if longName, exists := shortToLong[shortName]; exists {
+						flagName = longName
+					}
+				}
+
 				if flag, exists := cmd.Flags[flagName]; exists {
 					if i+1 < len(args) && !strings.HasPrefix(args[i+1], "-") {
 						flagValues[flagName] = args[i+1]
@@ -119,6 +139,10 @@ func (c *CLI) Run() {
 				}
 				lastResult = result.Value
 			}
+		}
+
+		if cmd.runFn != nil {
+			cmd.runFn()
 		}
 	}
 }
